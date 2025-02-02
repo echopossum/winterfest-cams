482dcd29500b9147b488cdc10336838a3414c71a
Echo <echopossum@gmail.com>
2025-02-02 16:25:13 -0500

    Basic camera layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ function App() {
   return (
     <div className="App">
       <Camera img="test" name="Camera 1" />
+      <Camera img="test" name="Camera 2" />
+      <Camera img="test" name="Camera 3" />
+      <Camera img="test" name="Camera 4" />
+      <Camera img="test" name="Camera 5" />
+      <Camera img="test" name="Camera 6" />
     </div>
   );
 }
